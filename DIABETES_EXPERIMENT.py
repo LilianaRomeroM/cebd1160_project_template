2e4675ac12c5d1742bf84d8bf22bc50de0bbe044
LilianaRomeroM <48030202+LilianaRomeroM@users.noreply.github.com>
2019-06-14 01:49:25 -0400

    Add files via upload

---
+++ DIABETES_EXPERIMENT.py
@@ -0,0 +1,234 @@
+import os
+import numpy as np
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import RFE
+import matplotlib.pyplot as plt
+import numpy as np 
+import pandas as pd
+import matplotlib
+import statsmodels.api as sm
+matplotlib.rcParams.update({'font.size': 12})
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from scipy import stats
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import LassoCV, LassoLarsCV, LassoLarsIC
+from sklearn.linear_model import Ridge, RidgeCV, Lasso, LassoCV
+from sklearn.metrics import mean_squared_error
+
+
+diabetes = load_diabetes()
+columns_names = diabetes.feature_names
+y = diabetes.target
+X = diabetes.data
+
+# Splitting features and target datasets into: train and test
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35)
+
+# Training a Linear Regression model with fit()
+from sklearn.linear_model import LinearRegression
+lm = LinearRegression()
+lm.fit(X_train, y_train)
+
+# Predicting the results for our test dataset
+predicted_values = lm.predict(X_test)
+
+# Printing the residuals: difference between real and predicted
+for (real, predicted) in list(zip(y_test, predicted_values)):
+    print(f"Value: {real:.2f}, pred: {predicted:.2f}, diff: {(real - predicted):.2f}")
+
+os.makedirs('EXPERPLOTS', exist_ok=True)
+
+# Plotting the residuals: difference between real and predicted
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(palette="inferno")
+residuals = y_test - predicted_values
+
+sns.scatterplot(y_test, predicted_values)
+plt.plot([0, 50], [0, 50], '--')
+plt.xlabel('Real Value')
+plt.ylabel('Predicted Value')
+plt.savefig('EXPERPLOTS/realvspredscatter.png')
+
+sns.scatterplot(y_test, residuals)
+plt.plot([50, 0], [0, 0], '--')
+plt.xlabel('Real Value')
+plt.ylabel('Residual (difference)')
+plt.savefig('EXPERPLOTS/residscatter.png')
+
+sns.distplot(residuals, bins=20, kde=False)
+plt.plot([0, 0], [50, 0], '--')
+plt.title('Residual (difference) Distribution')
+plt.savefig('EXPERPLOTS/distresids.png')
+
+# Understanding the error that we want to minimize
+from sklearn import metrics
+train_score=lm.score(X_train, y_train)
+test_score=lm.score(X_test, y_test)
+print ("linear regression train score:", train_score)
+print ("linear regression test score:", test_score)
+print(f"Printing MAE error(avg abs residual): {metrics.mean_absolute_error(y_test, predicted_values)}")
+print(f"Printing MSE error: {metrics.mean_squared_error(y_test, predicted_values)}")
+print(f"Printing RMSE error: {np.sqrt(metrics.mean_squared_error(y_test, predicted_values))}")
+
+X2 = sm.add_constant(X)
+est = sm.OLS(y, X2)
+est2 = est.fit()
+print(est2.summary())
+
+lm = LinearRegression()
+lm.fit(X,y)
+params = np.append(lm.intercept_,lm.coef_)
+predictions = lm.predict(X)
+
+newX = pd.DataFrame({"Constant":np.ones(len(X))}).join(pd.DataFrame(X))
+MSE = (sum((y-predictions)**2))/(len(newX)-len(newX.columns))
+
+
+var_b = MSE*(np.linalg.inv(np.dot(newX.T,newX)).diagonal())
+sd_b = np.sqrt(var_b)
+ts_b = params/ sd_b
+
+p_values =[2*(1-stats.t.cdf(np.abs(i),(len(newX)-1))) for i in ts_b]
+
+sd_b = np.round(sd_b,3)
+ts_b = np.round(ts_b,3)
+p_values = np.round(p_values,3)
+params = np.round(params,4)
+
+myDF3 = pd.DataFrame()
+myDF3["Coefficients"],myDF3["Standard Errors"],myDF3["t values"],myDF3["Probabilites"] = [params,sd_b,ts_b,p_values]
+print(myDF3)
+
+rr = Ridge(alpha=0.01) # higher the alpha value, more restriction on the coefficients; low alpha > more generalization, coefficients are barely
+# restricted and in this case linear and ridge regression resembles
+rr.fit(X_train, y_train)
+rr100 = Ridge(alpha=100) #  comparison with alpha value
+rr100.fit(X_train, y_train)
+Ridge_train_score = rr.score(X_train,y_train)
+Ridge_test_score = rr.score(X_test, y_test)
+Ridge_train_score100 = rr100.score(X_train,y_train)
+Ridge_test_score100 = rr100.score(X_test, y_test)
+print ("ridge regression train score low alpha:", Ridge_train_score)
+print ("ridge regression test score low alpha:", Ridge_test_score)
+print ("ridge regression train score high alpha:", Ridge_train_score100)
+print ("ridge regression test score high alpha:", Ridge_test_score100)
+plt.plot(rr.coef_,alpha=0.7,linestyle='none',marker='*',markersize=5,color='red',label=r'Ridge; $\alpha = 0.01$',zorder=7) # zorder for ordering the markers
+plt.plot(rr100.coef_,alpha=0.5,linestyle='none',marker='d',markersize=6,color='blue',label=r'Ridge; $\alpha = 100$') # alpha here is for transparency
+plt.xlabel('Coefficient Index',fontsize=16)
+plt.ylabel('Coefficient Magnitude',fontsize=16)
+plt.legend(fontsize=13,loc=4)
+
+
+# difference of lasso and ridge regression is that some of the coefficients can be zero i.e. some of the features are 
+# completely neglected
+lasso = Lasso()
+lasso.fit(X_train,y_train)
+train_score=lasso.score(X_train,y_train)
+test_score=lasso.score(X_test,y_test)
+coeff_used = np.sum(lasso.coef_!=0)
+print ("training score:", train_score) 
+print ("test score: ", test_score)
+print ("number of features used: ", coeff_used)
+lasso001 = Lasso(alpha=0.01, max_iter=10e5)
+lasso001.fit(X_train,y_train)
+train_score001=lasso001.score(X_train,y_train)
+test_score001=lasso001.score(X_test,y_test)
+coeff_used001 = np.sum(lasso001.coef_!=0)
+print ("training score for alpha=0.01:", train_score001) 
+print ("test score for alpha =0.01: ", test_score001)
+print ("number of features used: for alpha =0.01:", coeff_used001)
+lasso00001 = Lasso(alpha=0.0001, max_iter=10e5)
+lasso00001.fit(X_train,y_train)
+train_score00001=lasso00001.score(X_train,y_train)
+test_score00001=lasso00001.score(X_test,y_test)
+coeff_used00001 = np.sum(lasso00001.coef_!=0)
+print ("training score for alpha=0.0001:", train_score00001) 
+print ("test score for alpha =0.0001: ", test_score00001)
+print ("number of features used: for alpha =0.0001:", coeff_used00001)
+plt.subplot(1,2,1)
+plt.plot(lasso.coef_,alpha=0.7,linestyle='none',marker='*',markersize=5,color='red',label=r'Lasso; $\alpha = 1$',zorder=7) # alpha here is for transparency
+plt.plot(lasso001.coef_,alpha=0.5,linestyle='none',marker='d',markersize=6,color='blue',label=r'Lasso; $\alpha = 0.01$') # alpha here is for transparency
+
+plt.xlabel('Coefficient Index',fontsize=16)
+plt.ylabel('Coefficient Magnitude',fontsize=16)
+plt.legend(fontsize=13,loc=4)
+plt.subplot(1,2,2)
+plt.plot(lasso.coef_,alpha=0.7,linestyle='none',marker='*',markersize=5,color='red',label=r'Lasso; $\alpha = 1$',zorder=7) # alpha here is for transparency
+plt.plot(lasso001.coef_,alpha=0.5,linestyle='none',marker='d',markersize=6,color='blue',label=r'Lasso; $\alpha = 0.01$') # alpha here is for transparency
+plt.plot(lasso00001.coef_,alpha=0.8,linestyle='none',marker='v',markersize=6,color='black',label=r'Lasso; $\alpha = 0.00001$') # alpha here is for transparency
+plt.xlabel('Coefficient Index',fontsize=16)
+plt.ylabel('Coefficient Magnitude',fontsize=16)
+plt.legend(fontsize=13,loc=4)
+plt.tight_layout()
+plt.show()
+
+#another approach RIDGE
+alphas = 10**np.linspace(10,-2,100)*0.5
+ridge = Ridge()
+coefs = []
+
+for a in alphas:
+    ridge.set_params(alpha = a)
+    ridge.fit(X, y)
+    coefs.append(ridge.coef_)
+    
+np.shape(coefs)
+
+ax = plt.gca()
+ax.plot(alphas, coefs)
+ax.set_xscale('log')
+plt.axis('tight')
+plt.xlabel('alpha')
+plt.ylabel('weights')
+plt.show()
+
+ridgecv = RidgeCV(alphas = alphas, scoring = 'neg_mean_squared_error')
+ridgecv.fit(X_train, y_train)
+print('alpha', ridgecv.alpha_)
+
+ridge4 = Ridge(alpha = ridgecv.alpha_)
+ridge4.fit(X_train, y_train)
+mean_squared_error(y_test, ridge4.predict(X_test))
+ridge4.fit(X, y)
+print('MSE RIDGECROSSVAL', metrics.mean_squared_error(y_test, ridge4.predict(X_test)))
+print('coefficients', ridge.coef_)
+
+#NEW ATTEMPT WITH LASSO
+lasso = Lasso(max_iter = 10000)
+coefs = []
+
+for a in alphas:
+    lasso.set_params(alpha=a)
+    lasso.fit(X_train, y_train)
+    coefs.append(lasso.coef_)
+    
+ax = plt.gca()
+ax.plot(alphas*2, coefs)
+ax.set_xscale('log')
+plt.axis('tight')
+plt.xlabel('alpha')
+plt.ylabel('weights')
+plt.show()
+
+
+lassocv = LassoCV(alphas = None, cv = 10, max_iter = 100000)
+lassocv.fit(X_train, y_train)
+
+lasso.set_params(alpha=lassocv.alpha_)
+lasso.fit(X_train, y_train)
+print('MSE LASSO CROSSVAL', metrics.mean_squared_error(y_test, lasso.predict(X_test)))
+print('coefficients', lasso.coef_)
+plt.plot(range(len(columns_names)), lasso.coef_)
+plt.ylabel('coefficients')
+plt.xticks(range(len(columns_names)), columns_names, rotation=60) 
+plt.margins(0.02)
+plt.savefig('EXPERPLOTS/lassores.png')
+
+
